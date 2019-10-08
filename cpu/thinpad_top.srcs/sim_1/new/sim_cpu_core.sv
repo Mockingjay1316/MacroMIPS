7fce8276bc713b33d6737588ee290e9b211d0e5b
mockingjay <2017011417@secoder.net>
2019-10-08 10:56:23 +0800

    Update simulation files to test branch operations.

--- cpu/thinpad_top.srcs/sim_1/new/sim_cpu_core.sv
+++ cpu/thinpad_top.srcs/sim_1/new/sim_cpu_core.sv
@@ -25,27 +25,32 @@ module sim_cpu_core(
 
 );
 
-logic clk_10M;
+logic clk_10M, rst;
 logic[`INST_WIDTH-1:0] instr;
 
 initial begin
+    rst = 1'b1;
     clk_10M = 1'b0; # 10;
+    clk_10M = 1'b1; # 10;
+    rst = 1'b0;
     forever begin
         clk_10M = ~clk_10M; # 10;
     end
 end
 
 initial begin
-    instr = {`FUNCT_ADDIU, 5'b00000, 5'b00001, 16'h0002}; #100;
-    instr = {`FUNCT_ADDIU, 5'b00000, 5'b00010, 16'hFFFF}; #100;
-    instr = {`FUNCT_ADDIU, 5'b00000, 5'b00011, 16'hFFFF}; #100;
-    instr = {`FUNCT_ADDIU, 5'b00000, 5'b00101, 16'h7FFF}; #100;
-    instr = {`FUNCT_SPECIAL, 5'b00101, 5'b00001, 5'b00001, 5'b00000, `OP_ADDU}; #100;
+    #30;
+    instr = {`FUNCT_ADDIU, 5'b00000, 5'b00001, 16'h0002}; #20;
+    instr = {`FUNCT_BNE, 5'b00000, 5'b00001, 16'hFF0F}; #20;
+    instr = {`FUNCT_XORI, 5'b00000, 5'b00011, 16'hFFFF}; #20;
+    instr = {`FUNCT_ANDI, 5'b00000, 5'b00101, 16'h7FFF}; #20;
+    instr = {`FUNCT_SPECIAL, 5'b00101, 5'b00001, 5'b00001, 5'b00000, `OP_ADDU}; #20;
 end
 
 cpu_core cpu (
     .instruction(instr),
-    .clk_50M(clk_10M)
+    .clk_50M(clk_10M),
+    .reset_btn(rst)
 );
 
 endmodule
