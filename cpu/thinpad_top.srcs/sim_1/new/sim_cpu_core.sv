6633db47b4fceac845168a7e9b77a9c1fefd5152
Cao Dingyuan <2017011417@secoder.net>
2019-10-09 12:26:10 +0800

    Modify simulation file to test JALR.

--- cpu/thinpad_top.srcs/sim_1/new/sim_cpu_core.sv
+++ cpu/thinpad_top.srcs/sim_1/new/sim_cpu_core.sv
@@ -44,7 +44,8 @@ initial begin
     instr = {`OP_BNE, 5'b00000, 5'b00001, 16'hFF0F}; #20;
     instr = {`OP_XORI, 5'b00000, 5'b00011, 16'hFFFF}; #20;
     instr = {`OP_JAL, 5'b00000, 5'b00101, 16'h7FFF}; #20;
-    instr = {`OP_SPECIAL, 5'b11111, 5'b00000, 5'b00000, 5'b00000, `FUNCT_JR}; #20;
+    instr = {`OP_SPECIAL, 5'b00101, 5'b00001, 5'b00001, 5'b00000, `FUNCT_ADDU}; #20;
+    instr = {`OP_SPECIAL, 5'b11111, 5'b00000, 5'b00010, 5'b00000, `FUNCT_JALR}; #20;
     instr = {`OP_SPECIAL, 5'b00101, 5'b00001, 5'b00001, 5'b00000, `FUNCT_ADDU}; #20;
 end
 
