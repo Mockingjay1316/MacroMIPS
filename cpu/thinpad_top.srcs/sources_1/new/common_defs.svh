1a8f470626336242e5b894b4869255032a8d50e4
Cao Dingyuan <2017011417@secoder.net>
2019-10-12 02:14:08 +0800

    Add macros for LOAD & SAVE instructions. #11

--- cpu/thinpad_top.srcs/sources_1/new/common_defs.svh
+++ cpu/thinpad_top.srcs/sources_1/new/common_defs.svh
@@ -38,6 +38,11 @@ typedef enum logic[3:0] {
 `define OP_ORI          6'b001101
 `define OP_XORI         6'b001110
 `define OP_LUI          6'b001111
+`define OP_LB           6'b100000
+`define OP_LW           6'b100011
+`define OP_LBU          6'b100100
+`define OP_SB           6'b101000
+`define OP_SW           6'b101011
 
 `define FUNCT_SLL       6'b000000
 `define FUNCT_SLLV      6'b000100
