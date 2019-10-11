6c720b197bd5ee461aa6416a51e4a45bcfdffa01
Cao Dingyuan <2017011417@secoder.net>
2019-10-12 02:19:29 +0800

    Update control unit to decode load&save instructions. #11

--- cpu/thinpad_top.srcs/sources_1/new/ctrl/control_unit.sv
+++ cpu/thinpad_top.srcs/sources_1/new/ctrl/control_unit.sv
@@ -20,6 +20,10 @@ module control_unit (
     output  logic                       is_branch,                  //是否执行分支转移
     output  logic[`ADDR_WIDTH-1:0]      new_pc,                     //转移到的新的PC
 
+    output  logic                       load_from_mem, mem_data_write_en,
+    output  logic                       is_mem_data_read, mem_byte_en,
+    output  logic                       mem_sign_ext,
+    output  logic[`DATA_WIDTH-1:0]      mem_data,                   //写入内存的数据
     output  logic[4:0]                  stall
 );
 
@@ -35,29 +39,30 @@ assign sa = instr[10:6];
 
 assign stall = 5'b00000;
 
-logic[`ADDR_WIDTH-1:0] branch_new_pc, jump_new_pc, delay_slot_pc;
+logic[`ADDR_WIDTH-1:0] branch_new_pc, jump_new_pc, delay_slot_pc, ret_pc;
 logic[`DATA_WIDTH-1:0] rdata1, rdata2;
 
 assign delay_slot_pc = old_pc + 3'b100;
+assign ret_pc = old_pc + 4'b1000;
 assign branch_new_pc = {{14{imm_unext[15]}}, imm_unext[15:0], 2'b00} + delay_slot_pc;
 assign jump_new_pc   = {delay_slot_pc[31:28],  instr[25:0], 2'b00};
 
 always @(*) begin
-    if ((reg_raddr1 === ex_reg_waddr)
-        && (ex_reg_write_en === 1'b1)) begin
+    if ((reg_raddr1 == ex_reg_waddr)
+        && (ex_reg_write_en == 1'b1)) begin
         rdata1 <= ex_alu_result;
-    end else if ((reg_raddr1 === mem_reg_waddr)
-        && (mem_reg_write_en === 1'b1)) begin
+    end else if ((reg_raddr1 == mem_reg_waddr)
+        && (mem_reg_write_en == 1'b1)) begin
         rdata1 <= mem_reg_wdata;
     end else begin
         rdata1 <= reg_rdata1;
     end
 
-    if ((reg_raddr2 === ex_reg_waddr)
-        && (ex_reg_write_en === 1'b1)) begin
+    if ((reg_raddr2 == ex_reg_waddr)
+        && (ex_reg_write_en == 1'b1)) begin
         rdata2 <= ex_alu_result;
-    end else if ((reg_raddr2 === mem_reg_waddr)
-        && (mem_reg_write_en === 1'b1)) begin
+    end else if ((reg_raddr2 == mem_reg_waddr)
+        && (mem_reg_write_en == 1'b1)) begin
         rdata2 <= mem_reg_wdata;
     end else begin
         rdata2 <= reg_rdata2;
@@ -76,6 +81,11 @@ branch_res branch_res_r (
 always @(*) begin
     reg_write_en <= 1'b0;
     is_branch <= 1'b0;
+    load_from_mem <= 1'b0;
+    mem_byte_en <= 1'b0;
+    mem_sign_ext <= 1'b0;
+    mem_data_write_en <= 1'b0;
+    is_mem_data_read <= 1'b0;
     case(funct)
         /****************   Immediate   ********************/
         `OP_ADDIU: begin                                    //ADDIU
@@ -200,7 +210,7 @@ always @(*) begin
                     end
                 `FUNCT_JALR: begin                          //JALR
                     reg_write_en <= 1'b1;
-                    operand1     <= old_pc + 4'b1000;       //返回地址
+                    operand1     <= ret_pc;                 //返回地址
                     alu_op       <= ALU_NOP;                //ALU无需操作
                     is_branch    <= 1'b1;
                     new_pc       <= rdata1;
@@ -263,10 +273,65 @@ always @(*) begin
         `OP_JAL: begin                                      //JAL
             reg_write_en <= 1'b1;
             reg_waddr    <= 5'b11111;                       //需要把返回地址写进31号寄存器
-            operand1     <= old_pc + 4'b1000;               //返回地址
+            operand1     <= ret_pc;                         //返回地址
             alu_op       <= ALU_NOP;                        //ALU无需操作
             is_branch    <= 1'b1;
             new_pc       <= jump_new_pc;
+            end
+        `OP_LB: begin                                       //LB
+            alu_op <= ALU_ADD;                              //需要计算出访存的地址
+            operand1 <= rdata1;
+            operand2 <= {{16{imm_unext[15]}}, imm_unext[15:0]};
+            reg_waddr <= reg_raddr2;                        //访存结果保存的寄存器
+            reg_write_en <= 1'b1;                           //LOAD需要写寄存器
+            load_from_mem <= 1'b1;                          //需要从mem里load
+            mem_data_write_en <= 1'b0;                      //内存写使能置0
+            is_mem_data_read <= 1'b1;                       //数据读操作
+            mem_byte_en <= 1'b1;                            //需要字节使能
+            mem_sign_ext <= 1'b1;                           //读出来的字节需要符号拓展
+            end
+        `OP_LBU: begin                                      //LBU
+            alu_op <= ALU_ADD;                              //需要计算出访存的地址
+            operand1 <= rdata1;
+            operand2 <= {{16{imm_unext[15]}}, imm_unext[15:0]};
+            reg_waddr <= reg_raddr2;                        //访存结果保存的寄存器
+            reg_write_en <= 1'b1;                           //LOAD需要写寄存器
+            load_from_mem <= 1'b1;                          //需要从mem里load
+            mem_data_write_en <= 1'b0;                      //内存写使能置0
+            is_mem_data_read <= 1'b1;                       //数据读操作
+            mem_byte_en <= 1'b1;                            //需要字节使能
+            end
+        `OP_LW: begin                                       //LW
+            alu_op <= ALU_ADD;                              //需要计算出访存的地址
+            operand1 <= rdata1;
+            operand2 <= {{16{imm_unext[15]}}, imm_unext[15:0]};
+            reg_waddr <= reg_raddr2;                        //访存结果保存的寄存器
+            reg_write_en <= 1'b1;                           //LOAD需要写寄存器
+            load_from_mem <= 1'b1;                          //需要从mem里load
+            mem_data_write_en <= 1'b0;                      //内存写使能置0
+            is_mem_data_read <= 1'b1;                       //数据读操作
+            end
+        `OP_SB: begin                                       //SB
+            alu_op <= ALU_ADD;                              //需要计算出访存的地址
+            operand1 <= rdata1;
+            operand2 <= {{16{imm_unext[15]}}, imm_unext[15:0]};
+            mem_data <= rdata2;                             //要写入内存的数据
+            reg_write_en <= 1'b0;                           //SAVE不需要写寄存器
+            load_from_mem <= 1'b0;                          //需要从mem里load
+            mem_data_write_en <= 1'b1;                      //内存写使能置1
+            is_mem_data_read <= 1'b0;                       //数据写操作
+            mem_byte_en <= 1'b1;                            //需要字节使能
+            end
+        `OP_SW: begin                                       //SW
+            alu_op <= ALU_ADD;                              //需要计算出访存的地址
+            operand1 <= rdata1;
+            operand2 <= {{16{imm_unext[15]}}, imm_unext[15:0]};
+            mem_data <= rdata2;
+            reg_write_en <= 1'b0;                           //SAVE不需要写寄存器
+            load_from_mem <= 1'b0;                          //需要从mem里load
+            mem_data_write_en <= 1'b1;                      //内存写使能置1
+            is_mem_data_read <= 1'b0;                       //数据写操作
+            mem_byte_en <= 1'b1;                            //需要字节使能
             end
         default: begin
             
