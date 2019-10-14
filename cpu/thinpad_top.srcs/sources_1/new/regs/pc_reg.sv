bb7718b7c28cea999cf13c2cb966d0130e7f80ab
Cao Dingyuan <2017011417@secoder.net>
2019-10-14 19:31:45 +0800

    Fix pc register.

--- cpu/thinpad_top.srcs/sources_1/new/regs/pc_reg.sv
+++ cpu/thinpad_top.srcs/sources_1/new/regs/pc_reg.sv
@@ -4,6 +4,7 @@ module pc_reg (
     input   logic                       clk,            //时钟信号
     input   logic                       rst,            //pc复位
     input   logic                       stall,          //流水线暂停信号
+    input   logic                       mem_stall,
     input   logic                       write_en,       //写使能，高电平输入新的pc
     input   logic[`ADDR_WIDTH-1:0]      pc_in,          //pc输入
 
@@ -13,10 +14,10 @@ module pc_reg (
 always @(posedge clk) begin
     if (rst) begin
         pc_out <= 32'h80000000;
+    end else if (stall | mem_stall) begin
+        pc_out <= pc_out;
     end else if (write_en) begin
         pc_out <= pc_in;
-    end else if (stall) begin
-        pc_out <= pc_out;
     end else begin
         pc_out <= pc_out + 4;
     end
