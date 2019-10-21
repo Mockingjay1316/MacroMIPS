1ec6420481df2214bc70cdd437cc9986e578b8da
Cao Dingyuan <2017011417@secoder.net>
2019-10-21 20:35:53 +0800

    Simplify SRAM controller, make data latched. #15

--- cpu/thinpad_top.srcs/sources_1/new/peripheral/sram_controller.sv
+++ cpu/thinpad_top.srcs/sources_1/new/peripheral/sram_controller.sv
@@ -64,7 +64,7 @@ always @(*) begin
         
     end
 end
-/*
+
 always @(posedge peri_clk) begin
     if (rst) begin
         base_ram_ce_n <= 1'b1;
@@ -141,87 +141,6 @@ always @(posedge peri_clk) begin
             end
         end else begin
             data_read <= 32'hzzzzzzzz;
-        end
-    end
-end
-*/
-
-always @(*) begin
-    if (rst) begin
-        base_ram_ce_n <= 1'b1;
-        base_ram_oe_n <= 1'b1;
-        base_ram_we_n <= 1'b1;
-        ext_ram_ce_n  <= 1'b1;
-        ext_ram_oe_n  <= 1'b1;
-        ext_ram_we_n  <= 1'b1;
-    end else begin
-        base_ram_ce_n <= 1'b0;
-        base_ram_oe_n <= 1'b0;
-        base_ram_we_n <= 1'b1;
-        if (~mem_stall) begin
-            base_ram_addr <= pc[21:2];
-        end else begin
-            base_ram_addr <= data_addr[21:2];
-            if (~is_data_read) begin
-                base_ram_we_n <= ~data_write_en;
-                base_ram_oe_n <= 1'b0;
-                if (mem_byte_en) begin
-                    base_wdata <= {data_write[7:0], data_write[7:0], data_write[7:0], data_write[7:0]};
-                end else begin
-                    base_wdata <= data_write;
-                end
-            end
-        end
-
-        if (is_data_read) begin
-            ext_ram_ce_n <= 1'b0;
-            ext_ram_oe_n <= 1'b0;
-            ext_ram_we_n <= 1'b1;
-            ext_ram_addr <= data_addr[21:2];
-        end else begin
-            ext_ram_ce_n <= 1'b0;
-            ext_ram_oe_n <= 1'b0;
-            ext_ram_we_n <= ~data_write_en;
-            ext_ram_addr <= data_addr[21:2];
-            if (mem_byte_en) begin
-                ext_wdata <= {data_write[7:0], data_write[7:0], data_write[7:0], data_write[7:0]};
-            end else begin
-                ext_wdata <= data_write;
-            end
-        end
-
-        if (~mem_stall) begin
-            instr_read <= base_ram_data;
-        end else begin
-            instr_read <= 32'h00000000;
-        end
-
-        if (is_data_read) begin
-            if (mem_byte_en) begin
-                if (mem_sign_ext) begin
-                    case(data_addr[1:0])
-                        2'b00: data_read <= {{24{rdata[7]}}, rdata[7:0]};
-                        2'b01: data_read <= {{24{rdata[15]}}, rdata[15:8]};
-                        2'b10: data_read <= {{24{rdata[23]}}, rdata[23:16]};
-                        2'b11: data_read <= {{24{rdata[31]}}, rdata[31:24]};
-                        default: begin
-                            
-                        end
-                    endcase
-                end else begin
-                    case(data_addr[1:0])
-                        2'b00: data_read <= {24'h000000, rdata[7:0]};
-                        2'b01: data_read <= {24'h000000, rdata[15:8]};
-                        2'b10: data_read <= {24'h000000, rdata[23:16]};
-                        2'b11: data_read <= {24'h000000, rdata[31:24]};
-                        default: begin
-                            
-                        end
-                    endcase
-                end
-            end else begin
-                data_read <= rdata;
-            end
         end
     end
 end
