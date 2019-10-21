59e3a026891f4783f95c3884183b87c2a2506b93
Cao Dingyuan <2017011417@secoder.net>
2019-10-21 20:36:23 +0800

    Delete some useless signals. #15

--- cpu/thinpad_top.srcs/sources_1/new/cpu_core.sv
+++ cpu/thinpad_top.srcs/sources_1/new/cpu_core.sv
@@ -12,22 +12,6 @@ module cpu_core (
     output  logic[7:0]  dpy0,               //数码管低位信号，包括小数点，输出1点亮
     output  logic[7:0]  dpy1,               //数码管高位信号，包括小数点，输出1点亮
 
-    //BaseRAM信号
-    inout   logic[31:0] base_ram_data,      //BaseRAM数据，低8位与CPLD串口控制器共享
-    output  logic[19:0] base_ram_addr,      //BaseRAM地址
-    output  logic[3:0]  base_ram_be_n,      //BaseRAM字节使能，低有效。如果不使用字节使能，请保持为0
-    output  logic       base_ram_ce_n,      //BaseRAM片选，低有效
-    output  logic       base_ram_oe_n,      //BaseRAM读使能，低有效
-    output  logic       base_ram_we_n,      //BaseRAM写使能，低有效
-
-    //ExtRAM信号
-    inout   logic[31:0] ext_ram_data,       //ExtRAM数据
-    output  logic[19:0] ext_ram_addr,       //ExtRAM地址
-    output  logic[3:0]  ext_ram_be_n,       //ExtRAM字节使能，低有效。如果不使用字节使能，请保持为0
-    output  logic       ext_ram_ce_n,       //ExtRAM片选，低有效
-    output  logic       ext_ram_oe_n,       //ExtRAM读使能，低有效
-    output  logic       ext_ram_we_n,       //ExtRAM写使能，低有效
-
     output  logic[`ADDR_WIDTH-1:0]      pc_out, mem_addr,
     output  logic[`DATA_WIDTH-1:0]      mem_wdata, reg_out,
     output  logic[4:0]                  mem_ctrl_signal,
