0de3a9847f17a13b92853c9cf7aeac24e66a77c2
Cao Dingyuan <2017011417@secoder.net>
2019-11-03 16:17:10 +0800

    Fixed that when accessing UART, Ext RAM will also be accessed.

--- cpu/thinpad_top.srcs/sources_1/new/peripheral/sram_controller.sv
+++ cpu/thinpad_top.srcs/sources_1/new/peripheral/sram_controller.sv
@@ -74,6 +74,10 @@ always @(posedge peri_clk) begin
         ext_ram_ce_n  <= 1'b1;
         ext_ram_oe_n  <= 1'b1;
         ext_ram_we_n  <= 1'b1;
+    end else if (data_addr >= 32'hbfd003f8) begin
+        ext_ram_ce_n  <= 1'b1;
+        ext_ram_oe_n  <= 1'b1;
+        ext_ram_we_n  <= 1'b1;
     end else if (main_clk == 1'b1) begin
         base_ram_ce_n <= 1'b0;
         base_ram_oe_n <= 1'b0;
