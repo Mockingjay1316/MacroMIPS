c15be646314842e9ee42cad81aaffec2a33d0f5f
Cao Dingyuan <2017011417@secoder.net>
2019-11-04 22:56:25 +0800

    Fix a misplacement of exception code. #21

--- cpu/thinpad_top.srcs/sources_1/new/ctrl/excep_handler.sv
+++ cpu/thinpad_top.srcs/sources_1/new/ctrl/excep_handler.sv
@@ -3,6 +3,7 @@
 module excep_handler (
     input   excep_info_t        mem_excep_info,     //输入异常信息
     input   logic               hardware_int,
+    input   logic[31:0]         Status,             //Status寄存器
     output  logic[7:0]          excep_code,         //输出异常码
     output  logic               is_excep,
     output  logic[31:0]         EPC_out,
@@ -22,11 +23,21 @@ always_comb begin
     if (mem_excep_info.is_excep) begin
         is_excep <= 1'b1;
         flush <= 5'b01110;
-        excep_code <= 8'd8;
+        excep_code[6:2] <= 5'd8;
     end else if (hardware_int) begin
         is_excep <= 1'b1;
         flush <= 5'b01110;
-        excep_code <= 8'd0;
+        excep_code[6:2] <= 5'd0;
+    end
+
+    if (Status[1] != 1'b0 || Status[2] != 1'b0) begin
+        is_excep <= 1'b0;
+        flush <= 5'b00000;
+        excep_code <= 8'b00000000;
+    end else if (Status[0] == 1'b0) begin
+        is_excep <= 1'b0;
+        flush <= 5'b00000;
+        excep_code <= 8'b00000000;
     end
 end
 
