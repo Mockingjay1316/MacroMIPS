8d975965795972ab63d0846b7daf00f238b9c4c9
胡锦毅 <hujy17@mails.tsinghua.edu.cn>
2019-11-06 19:52:34 +0800

    Fix bug in test_cpu. #18

--- testbench/cpu/test_cpu.sv
+++ testbench/cpu/test_cpu.sv
@@ -1,6 +1,6 @@
 `include "common_defs.svh"
 
-`define PATH_PREFIX1 "/testcase/"
+`define PATH_PREFIX1 "//Mac/Home/Desktop/MarcoMips/testbench/cpu/testcase/"
 module test_cpu();
 
 logic cpu_clk, peri_clk;
@@ -13,9 +13,9 @@ logic[4:0] mem_ctrl_signal;
 logic [`INST_WIDTH - 1:0] inst_mem[4095:0];
 
 
-cpu_core cpu (
+cpu_core cpu(
     .instruction(instr),
-    .clk_50M(clk_50M),
+    .clk_50M(cpu_clk),
     .reset_btn(~rst),
     .pc_out(pc),
 
@@ -26,31 +26,10 @@ cpu_core cpu (
     .mem_stall(mem_stall)
 );
 
-main_pll pll (
-    .clk_in1(clk_50M),
-    .clk_out2(peri_clk),
-    .locked(rst)
-);
-
-sram_controller sram_ctrl (
-    .main_clk(clk_50M),
-    .peri_clk(peri_clk),
-    .rst(~rst),
-    .pc(pc),
-    //.instr_read(instr),
-    .data_write_en(mem_ctrl_signal[3]),
-    .is_data_read(mem_ctrl_signal[2]),
-    .mem_byte_en(mem_ctrl_signal[1]),
-    .mem_sign_ext(mem_ctrl_signal[0]),
-    .data_addr(mem_addr),
-    .data_write(mem_wdata),
-    .data_read(mem_rdata),
-    .mem_stall(mem_stall)
-);
-
 task judge(input integer fans, input integer cycle, input string out, input check_cyc);
 	string ans, out_with_cyc;
 	$fscanf(fans, "%s\n", ans);
+	
 	$sformat(out_with_cyc, "[%0d]%s", cycle, out);
 	if(check_cyc) out = out_with_cyc;
 	if(out != ans && ans != "skip")
@@ -63,51 +42,53 @@ task judge(input integer fans, input integer cycle, input string out, input chec
 	end
 endtask
 
-assign reg_wr1 = cpu_core.reg_file;
+assign reg_waddr = cpu.wb_reg_waddr;
+assign reg_wdata = cpu.wb_reg_wdata;
+
 task unittest(
 	input [128*8 - 1:0] file_name,
 	input check_cyc,
 	input fpu
 );
 
-	integer i, fans, cycle = 0;
-	integer is_event = 0;
+	integer i, fans, count, mem_index = 0;
 	string ans, out, info;
 
 	
 	for(i = 0; i < $size(inst_mem); i = i + 1)
 	inst_mem[i] = 32'h0;
-	fans = $fopen({ `PATH_PREFIX1, file_name, ".ans"}, "r");
-	if (fans)
 	begin
-		$readmemh({ `PATH_PREFIX1, name, ".mem" }, inst_mem);
+		$readmemh({ file_name, ".mem" }, inst_mem);
 	end
-	inital begin
-		cpu_clk = 1'b0;
-		forever #10 cpu_clk = ~cpu_clk;
+	fans = $fopen({ `PATH_PREFIX1, file_name, ".ans"}, "r");
+	
+	begin
+		cpu_clk = 1'b0;  #10;
+		cpu_clk = 1'b1;  #10;
+		forever begin
+		  cpu_clk = ~cpu_clk; #10;
+		end
 	end
-	initial begin
-		#1530
+	begin
 		for(i = 0; i < $size(inst_mem); i = i + 1)
-		instr = inst_mem[i];    #20
+		instr = inst_mem[i]; #20;	
 	end
 
-	$display("======= unittest: %0s =======", name);
+	$display("======= unittest: %0s =======", file_name);
 
 	count = 0;
 	while(!$feof(fans))
 	begin @(negedge cpu_clk);
-		$sformat(out, "$%0d=0x%x", reg_wr1.waddr, reg_wr1.wdata);
-		judge(fans, cycle, out, check_cyc);
-
+	    count = count + 1;
+		$sformat(out, "$%0d=0x%x", reg_waddr, reg_wdata);
+		judge(fans, count, out, check_cyc);
 	end
 	$display("[OK] %0s\n", file_name);
 endtask
 
 initial begin
-	wait (clk.rst == 1'b0);
-	unittest('inst_ori',0,0);
-	unittest('inst_logic',0,0);
+	unittest("inst_ori",0,0);
+	unittest("inst_logic",0,0);
 end
 
 endmodule
