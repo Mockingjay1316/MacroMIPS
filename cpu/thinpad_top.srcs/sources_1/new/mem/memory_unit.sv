61827caea35a6cb4c07f62704c5addc3165db58d
Cao Dingyuan <2017011417@secoder.net>
2019-12-01 13:58:38 +0800

    Rewrite MMU. #26

--- cpu/thinpad_top.srcs/sources_1/new/mem/memory_unit.sv
+++ cpu/thinpad_top.srcs/sources_1/new/mem/memory_unit.sv
@@ -7,66 +7,50 @@ module memory_unit (
     input   logic[31:0]             index, EntryHi, PageMask, EntryLo1, EntryLo0,
 
     output  logic                   pc_tlb_hit, mem_tlb_hit,
-    output  logic[31:0]             pc_out, mem_addr_out
+    output  logic[31:0]             pc_out, mem_addr_out, tlbp_index,
+    output  mmu_res_t               pc_mmu_result, data_mmu_result,
+    output  tlb_entry_t             tlb_rdata
 );
 
-tlb_entry_t tlb_entries[`MMU_SIZE_NUM:0];
-logic[`MMU_SIZE_NUM:0] pc_res, mem_addr_res;
-integer i, j;
+logic pc_mapped, data_mapped;
+tlb_res_t pc_result, data_result;
 
-always_comb begin
-    if (rst) begin
-        pc_tlb_hit <= 1'b0;
-        mem_tlb_hit <= 1'b0;
-    end else begin
-        if (pc_in >= 32'h80000000 && pc_in < 32'hC0000000) begin
-            pc_out <= pc_in;
-            pc_tlb_hit <= 1'b1;
-        end else begin
-            for (i = 0; i <= `MMU_SIZE; ++i) begin
-                if (pc_in[31:13] & {3'b111, ~tlb_entries[i].PageMask[28:13]} == tlb_entries[i].VPN2) begin
-                    pc_res[i] <= 1'b1;
-                    pc_tlb_hit <= 1'b1;
-                    if (pc_in[12] == 1'b0) begin
-                        pc_out <= {tlb_entries[i].PFN0, pc_in[11:0]};
-                    end else if (pc_in[12] == 1'b1) begin
-                        pc_out <= {tlb_entries[i].PFN1, pc_in[11:0]};
-                    end
-                end else begin
-                    pc_res[i] <= 1'b0;
-                end
-            end
-        end
-        if (mem_addr_in >= 32'h80000000 && mem_addr_in < 32'hC0000000) begin
-            mem_addr_out <= mem_addr_in;
-            mem_tlb_hit <= 1'b1;
-        end else begin
-            for (j = 0; j <= `MMU_SIZE; ++j) begin
-                if (mem_addr_in[31:13] & {3'b111, ~tlb_entries[j].PageMask[28:13]} == tlb_entries[j].VPN2) begin
-                    mem_addr_res[j] <= 1'b1;
-                    mem_tlb_hit <= 1'b1;
-                    if (mem_addr_in[12] == 1'b0) begin
-                        mem_addr_out <= {tlb_entries[j].PFN0, mem_addr_in[11:0]};
-                    end else if (mem_addr_in[12] == 1'b1) begin
-                        mem_addr_out <= {tlb_entries[j].PFN1, mem_addr_in[11:0]};
-                    end
-                end else begin
-                    mem_addr_res[j] <= 1'b0;
-                end
-            end
-        end
-    end
-end
+assign pc_mapped = (~pc_in[31] || pc_in[31:30] == 2'b11);
+assign data_mapped = (~mem_addr_in[31] || mem_addr_in[31:30] == 2'b11);
 
-always @(posedge clk) begin
-    if (tlb_write_en) begin
-        tlb_entries[index[`MMU_SIZE_NUM:0]].VPN2 <= EntryHi[31:13];
-        tlb_entries[index[`MMU_SIZE_NUM:0]].PageMask <= PageMask;
-        tlb_entries[index[`MMU_SIZE_NUM:0]].PFN0 <= EntryLo0[31:6];
-        tlb_entries[index[`MMU_SIZE_NUM:0]].PFN0_fl <= EntryLo0[5:1];
-        tlb_entries[index[`MMU_SIZE_NUM:0]].PFN1 <= EntryLo1[31:6];
-        tlb_entries[index[`MMU_SIZE_NUM:0]].PFN1_fl <= EntryLo1[5:1];
-    end
-end
+assign pc_mmu_result.dirty = 1'b0;
+assign pc_mmu_result.miss = pc_mapped & pc_result.miss;
+assign pc_mmu_result.illegal = 1'b0;
+assign pc_mmu_result.valid = ~pc_mapped | pc_result.valid;
+assign pc_mmu_result.paddr = pc_mapped ? pc_result.paddr : pc_in;
+assign pc_mmu_result.vaddr = pc_in;
+
+assign data_mmu_result.dirty = ~data_mapped | data_result.dirty;
+assign data_mmu_result.miss = data_mapped & data_result.miss;
+assign data_mmu_result.illegal = 1'b0;
+assign data_mmu_result.valid = ~data_mapped | data_result.valid;
+assign data_mmu_result.paddr = data_mapped ? data_result.paddr : mem_addr_in;
+assign data_mmu_result.vaddr = mem_addr_in;
+
+tlb_entry_t tlb_wdata;
+assign tlb_wdata = {EntryHi[31:13], EntryHi[7:0], PageMask, EntryLo0[0], EntryLo0[31:6], EntryLo1[31:6], EntryLo0[5:1], EntryLo1[5:1]};
+
+tlb tlb_r (
+    .clk,
+    .rst,
+    .pc_vaddr(pc_in),
+    .data_vaddr(mem_addr_in),
+    .pc_result,
+    .data_result,
+    .asid(8'b0),
+
+    .tlb_index(index[3:0]),
+    .tlb_write_en,
+    .tlb_wdata,
+    .tlb_rdata,
+
+    .tlbp_entryhi(EntryHi),
+    .tlbp_index
+);
 
 endmodule
