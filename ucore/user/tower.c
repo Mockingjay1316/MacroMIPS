516290c85cb8bb86ef2129598e06ea87d1ede735
chenweize1998 <chenweize1998@gmail.com>
2019-12-03 16:47:40 +0800

    fix some bugs in Makefile. fixes #31

--- ucore/user/tower.c
+++ ucore/user/tower.c
@@ -6,12 +6,47 @@
 // #include "map.h"
 
 #define printf(...)         fprintf(1, __VA_ARGS__)
-#define BOARD_SIZE 13
-#define MAX_SEQ_SIZE 30
+#define BOARD_SIZE          13
+#define MAX_SEQ_SIZE        30
+
+#define WALL                '#'
+#define BLUE_BOTTLE         'l'
+#define RED_BOTTLE          'h'
+#define RED_GATE            'r'
+#define BLUE_GATE           'b'
+#define YELLOW_GATE         'y'
+#define YELLOW_KEY          'k'
+#define BLUE_KEY            'e'
+#define RED_KEY             'd'
+#define BLUE_GEM            'o'
+#define RED_GEM             'p'
+#define SLIME               'm'
+#define BAT                 'f'
+#define SKELETON            'g'
+#define SKELETON_GENERAL    'j'
+#define WIZARD              's'
+#define ROAD                ' '
+#define UP_STAIR            't'
+#define DOWN_STAIR          'w'
+
+typedef struct Hero {
+    int x;
+    int y;
+    int hp;
+    int attack;
+    int defence;
+    int money;
+    int red_key_num;
+    int blue_key_num;
+    int yellow_key_num;
+}Hero;
 
 char map[BOARD_SIZE][BOARD_SIZE][MAX_SEQ_SIZE];
+char raw_map[BOARD_SIZE][BOARD_SIZE];
+Hero hero = {6, 11, 400, 15, 10, 0, 0, 0, 0};
+int layer = 1;
 
-void import(char* file_name, char map[BOARD_SIZE][BOARD_SIZE][MAX_SEQ_SIZE]) {
+void import(char* file_name) {
     // FILE* file = fopen(file_name, "r");
     int file = open(file_name, O_RDONLY);
     char buffer[BOARD_SIZE - 1];
@@ -22,48 +57,53 @@ void import(char* file_name, char map[BOARD_SIZE][BOARD_SIZE][MAX_SEQ_SIZE]) {
             strcpy(map[0][i], "██");
             strcpy(map[BOARD_SIZE-1][i], "██");
             strcpy(map[i][BOARD_SIZE-1], "██");
+            raw_map[i][0] = WALL;
+            raw_map[0][i] = WALL;
+            raw_map[BOARD_SIZE-1][i] = WALL;
+            raw_map[i][BOARD_SIZE-1] = WALL;
         }
         int y = 0;
         while ((ret = read(file, buffer, sizeof(buffer))) != 0) {
         // while((ret = fread(buffer, sizeof(char), BOARD_SIZE-1, file)) != 0) {
             for (int x = 0; x < BOARD_SIZE - 2; ++x) {  
-                if ('#' == buffer[x]) {                                  // 墙壁
+                raw_map[y + 1][x + 1] = buffer[x];
+                if (WALL == buffer[x]) {                                  // 墙壁
                     strcpy(map[y + 1][x + 1], "██");
-                } else if ('l' == buffer[x]) {                           // 蓝血瓶
+                } else if (BLUE_BOTTLE == buffer[x]) {                    // 蓝血瓶
                     print_blue(map, x + 1, y + 1, "血");
-                } else if ('h' == buffer[x]) {                           // 红血瓶
+                } else if (RED_BOTTLE == buffer[x]) {                     // 红血瓶
                     print_red(map, x + 1, y + 1, "血");
-                } else if ('r' == buffer[x]) {                           // 红门
+                } else if (RED_GATE == buffer[x]) {                       // 红门
                     print_red(map, x + 1, y + 1, "〓");
-                } else if ('b' == buffer[x]) {                           // 蓝门
+                } else if (BLUE_GATE == buffer[x]) {                      // 蓝门
                     print_blue(map, x + 1, y + 1, "〓");
-                } else if ('y' == buffer[x]) {                           // 黄门
+                } else if (YELLOW_GATE == buffer[x]) {                    // 黄门
                     print_yellow(map, x + 1, y + 1, "〓");
-                } else if ('k' == buffer[x]) {                           // 黄钥匙
+                } else if (YELLOW_KEY == buffer[x]) {                     // 黄钥匙
                     print_yellow(map, x + 1, y + 1, "♀ ");
-                } else if ('e' == buffer[x]) {                           // 蓝钥匙
+                } else if (BLUE_KEY == buffer[x]) {                       // 蓝钥匙
                     print_blue(map, x + 1, y + 1, "♀ ");
-                } else if ('d' == buffer[x]) {                           // 红钥匙
+                } else if (RED_KEY == buffer[x]) {                        // 红钥匙
                     print_red(map, x + 1, y + 1, "♀ ");
-                } else if ('o' == buffer[x]) {                           // 蓝宝石
+                } else if (BLUE_GEM == buffer[x]) {                       // 蓝宝石
                     print_blue(map, x + 1, y + 1, "◆ ");
-                } else if ('p' == buffer[x]) {                           // 红宝石
+                } else if (RED_GEM == buffer[x]) {                        // 红宝石
                     print_red(map, x + 1, y + 1, "◆ ");
-                } else if ('m' == buffer[x]) {                           // 史莱姆
+                } else if (SLIME == buffer[x]) {                          // 史莱姆
                     strcpy(map[y + 1][x + 1], "史");
-                } else if ('f' == buffer[x]) {                           // 蝙蝠
+                } else if (BAT == buffer[x]) {                            // 蝙蝠
                     strcpy(map[y + 1][x + 1], "蝠");
-                } else if ('g' == buffer[x]) {                           // 骷髅
+                } else if (SKELETON == buffer[x]) {                       // 骷髅
                     strcpy(map[y + 1][x + 1], "骷");
-                } else if ('j' == buffer[x]) {                           // 骷髅将军
+                } else if (SKELETON_GENERAL == buffer[x]) {               // 骷髅将军
                     strcpy(map[y + 1][x + 1], "军");
-                } else if ('s' == buffer[x]) {                           // 法师
+                } else if (WIZARD == buffer[x]) {                         // 法师
                     strcpy(map[y + 1][x + 1], "法");
-                } else if (' ' == buffer[x]) {                           // 道路
+                } else if (ROAD == buffer[x]) {                           // 道路
                     strcpy(map[y + 1][x + 1], "  ");
-                } else if ('t' == buffer[x]) {                           // 上楼梯
+                } else if (UP_STAIR == buffer[x]) {                       // 上楼梯
                     strcpy(map[y + 1][x + 1], "↑ ");
-                } else if ('w' == buffer[x]) {                           // 下楼梯
+                } else if (DOWN_STAIR == buffer[x]) {                     // 下楼梯
                     strcpy(map[y + 1][x + 1], "↓ ");
                 }
             }
@@ -71,8 +111,11 @@ void import(char* file_name, char map[BOARD_SIZE][BOARD_SIZE][MAX_SEQ_SIZE]) {
         }    
     }
 }
-void draw(char map[BOARD_SIZE][BOARD_SIZE][MAX_SEQ_SIZE]){
+void draw(){
     printf("%c[%d;%dH",27,1,1);
+    printf("HP:%d\t攻击力:%d\t防御力:%d\n金钱:%d\t层数:%d\n黄钥匙:%d\t蓝钥匙:%d\t红钥匙:%d\n\n", 
+            hero.hp, hero.attack, hero.defence, hero.money, layer, hero.yellow_key_num,
+            hero.blue_key_num, hero.red_key_num);
     for (int y = 0; y < BOARD_SIZE; ++y) {
         for (int x = 0; x < BOARD_SIZE; ++x) {
             printf("%s", map[y][x]);
@@ -108,10 +151,53 @@ void print_yellow(char map[BOARD_SIZE][BOARD_SIZE][MAX_SEQ_SIZE], int x, int y, 
     strcpy(map[y][x], buf);
 } 
 
+bool get_user_input(int *x_direction, int *y_direction) {
+    char c;
+    int ret;
+    bool flag = 0;
+    if ((ret = read(0, &c, sizeof(char))) < 0) {
+        return flag;
+    }
+    switch (c)
+    {
+    case 'w':
+        if (*y_direction != 1) {
+            flag = 1;
+            *y_direction = -1;
+            *x_direction = 0;
+        }
+        break;
+    case 's':
+        if (*y_direction != -1) {
+            flag = 1;
+            *y_direction = 1;
+            *x_direction = 0;
+        }
+        break;
+    case 'a':
+        if (*x_direction != 1) {
+            flag = 1;
+            *x_direction = -1;
+            *y_direction = 0;
+        }
+        break;
+    case 'd':
+        if (*x_direction != -1) {
+            flag = 1;
+            *x_direction = 1;
+            *y_direction = 0;
+        }
+        break;
+    default:
+        break;
+    }
+    return flag;
+}
+
 int main() {
     printf("\e[1;1H\e[2J");
-    import("map_1.txt", map);
-    draw(map);
-    // getchar();
+    import("map_1.txt");
+    draw();
+    
     return 0;
 }
