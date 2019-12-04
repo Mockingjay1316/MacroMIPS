bb6d38b5c1bec41407fe980d1b9c5a6e6c1cddf0
chenweize1998 <chenweize1998@gmail.com>
2019-12-04 20:43:54 +0800

    [tower] can reuse map now. fixes #31

--- ucore/user/tower.c
+++ ucore/user/tower.c
@@ -10,7 +10,7 @@
 #define true                1
 
 #define BOARD_SIZE          13
-#define MAX_SEQ_SIZE        30
+#define MAX_SEQ_SIZE        14
 
 #define WALL                '#'
 #define BLUE_BOTTLE         'l'
@@ -58,8 +58,9 @@ typedef struct Monster {
     int defence;
 }Monster;
 
-char map[BOARD_SIZE][BOARD_SIZE][MAX_SEQ_SIZE];
-char raw_map[BOARD_SIZE][BOARD_SIZE];
+char map[9][BOARD_SIZE][BOARD_SIZE][MAX_SEQ_SIZE];
+char raw_map[9][BOARD_SIZE][BOARD_SIZE];
+bool map_has_load[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 int up_x[10] = {6, 1, 2, 11, 2, 1, 11, 1, 6, 1};
 int up_y[10] = {11, 2, 11, 10, 11, 2, 10, 2, 2, 10};
 int down_x[10] = {2, 1, 10, 1, 1, 11, 1, 6, 1};
@@ -75,22 +76,23 @@ void import(char* file_name) {
     if (file >= 0) {
         int ret;
         for (int i = 0; i < BOARD_SIZE; ++i) {
-            strcpy(map[i][0], "██");
-            strcpy(map[0][i], "██");
-            strcpy(map[BOARD_SIZE-1][i], "██");
-            strcpy(map[i][BOARD_SIZE-1], "██");
-            raw_map[i][0] = WALL;
-            raw_map[0][i] = WALL;
-            raw_map[BOARD_SIZE-1][i] = WALL;
-            raw_map[i][BOARD_SIZE-1] = WALL;
+            strcpy(map[layer-1][i][0], "██");
+            strcpy(map[layer-1][0][i], "██");
+            strcpy(map[layer-1][BOARD_SIZE-1][i], "██");
+            strcpy(map[layer-1][i][BOARD_SIZE-1], "██");
+            raw_map[layer-1][i][0] = WALL;
+            raw_map[layer-1][0][i] = WALL;
+            raw_map[layer-1][BOARD_SIZE-1][i] = WALL;
+            raw_map[layer-1][i][BOARD_SIZE-1] = WALL;
         }
         int y = 0;
         while ((ret = read(file, buffer, sizeof(buffer))) != 0) {
         // while((ret = fread(buffer, sizeof(char), BOARD_SIZE-1, file)) != 0) {
+            // printf("%d\n", y);
             for (int x = 0; x < BOARD_SIZE - 2; ++x) {  
-                raw_map[y + 1][x + 1] = buffer[x];
+                raw_map[layer-1][y + 1][x + 1] = buffer[x];
                 if (WALL == buffer[x]) {                                  // 墙壁
-                    strcpy(map[y + 1][x + 1], "██");
+                    strcpy(map[layer-1][y + 1][x + 1], "██");
                 } else if (BLUE_BOTTLE == buffer[x]) {                    // 蓝血瓶
                     print_blue(x + 1, y + 1, "血");
                 } else if (RED_BOTTLE == buffer[x]) {                     // 红血瓶
@@ -112,24 +114,24 @@ void import(char* file_name) {
                 } else if (RED_GEM == buffer[x]) {                        // 红宝石
                     print_red(x + 1, y + 1, "◆ ");
                 } else if (SLIME == buffer[x]) {                          // 史莱姆
-                    strcpy(map[y + 1][x + 1], "史");
+                    strcpy(map[layer-1][y + 1][x + 1], "史");
                 } else if (BAT == buffer[x]) {                            // 蝙蝠
-                    strcpy(map[y + 1][x + 1], "蝠");
+                    strcpy(map[layer-1][y + 1][x + 1], "蝠");
                 } else if (SKELETON == buffer[x]) {                       // 骷髅
-                    strcpy(map[y + 1][x + 1], "骷");
+                    strcpy(map[layer-1][y + 1][x + 1], "骷");
                 } else if (SKELETON_GENERAL == buffer[x]) {               // 骷髅将军
-                    strcpy(map[y + 1][x + 1], "军");
+                    strcpy(map[layer-1][y + 1][x + 1], "军");
                 } else if (WIZARD == buffer[x]) {                         // 法师
-                    strcpy(map[y + 1][x + 1], "法");
+                    strcpy(map[layer-1][y + 1][x + 1], "法");
                 } else if (ROAD == buffer[x]) {                           // 道路
-                    strcpy(map[y + 1][x + 1], "  ");
+                    strcpy(map[layer-1][y + 1][x + 1], "  ");
                 } else if (UP_STAIR == buffer[x]) {                       // 上楼梯
-                    strcpy(map[y + 1][x + 1], "↑ ");
+                    strcpy(map[layer-1][y + 1][x + 1], "↑ ");
                 } else if (DOWN_STAIR == buffer[x]) {                     // 下楼梯
-                    strcpy(map[y + 1][x + 1], "↓ ");
+                    strcpy(map[layer-1][y + 1][x + 1], "↓ ");
                 } else if (SHOP == buffer[x]) {
-                    strcpy(map[y + 1][x + 1], "商");
-                    strcpy(map[y + 1][x + 2], "店");
+                    strcpy(map[layer-1][y + 1][x + 1], "商");
+                    strcpy(map[layer-1][y + 1][x + 2], "店");
                     ++x;
                 } else if (SWORD == buffer[x]) {
                     // strcpy(map[y + 1][x + 1], "⚔ ");
@@ -142,6 +144,7 @@ void import(char* file_name) {
             ++y;
         }    
     }
+    map_has_load[layer - 1] = true;
 }
 void draw(){
     printf("%c[%d;%dH",27,1,1);
@@ -150,7 +153,7 @@ void draw(){
             hero.blue_key_num, hero.red_key_num);
     for (int y = 0; y < BOARD_SIZE; ++y) {
         for (int x = 0; x < BOARD_SIZE; ++x) {
-            printf("%s", map[y][x]);
+            printf("%s", map[layer-1][y][x]);
         }
         printf("\n");
     }
@@ -162,7 +165,7 @@ void print_red(int x, int y, char* c) {
     // strcpy(map[y][x], buf);
     strcat("\033[31m", c, buf);
     strcat(buf, "\033[0m", buf);
-    strcpy(map[y][x], buf);
+    strcpy(map[layer-1][y][x], buf);
 }
 
 void print_blue(int x, int y, char* c) {
@@ -171,7 +174,7 @@ void print_blue(int x, int y, char* c) {
     // strcpy(map[y][x], buf);
     strcat("\033[34m", c, buf);
     strcat(buf, "\033[0m", buf);
-    strcpy(map[y][x], buf);
+    strcpy(map[layer-1][y][x], buf);
 }
 
 void print_yellow(int x, int y, char* c) {
@@ -180,7 +183,7 @@ void print_yellow(int x, int y, char* c) {
     // strcpy(map[y][x], buf);
     strcat("\033[33m", c, buf);
     strcat(buf, "\033[0m", buf);
-    strcpy(map[y][x], buf);
+    strcpy(map[layer-1][y][x], buf);
 } 
 
 void print_green(int x, int y, char* c) {
@@ -189,7 +192,7 @@ void print_green(int x, int y, char* c) {
     // strcpy(map[y][x], buf);
     strcat("\033[36m", c, buf);
     strcat(buf, "\033[0m", buf);
-    strcpy(map[y][x], buf);
+    strcpy(map[layer-1][y][x], buf);
 } 
 
 
@@ -324,9 +327,11 @@ void update_layer(int delta) {
     char tmp[2];
     tmp[0] = '0' + layer;
     tmp[1] = '\0';
-    strcat("map_", tmp, file_name);
-    strcat(file_name, ".txt", file_name);
-    import(file_name);
+    if (!map_has_load[layer - 1]) {
+        strcat("map_", tmp, file_name);
+        strcat(file_name, ".txt", file_name);
+        import(file_name);
+    }
     if (delta > 0) {
         hero.x = up_x[layer - 1];
         hero.y = up_y[layer - 1];
@@ -388,14 +393,14 @@ bool move() {
     bool update = false;
     bool ret = false;
     bool new_layer = false;
-    switch (raw_map[y_result][x_result])
+    switch (raw_map[layer-1][y_result][x_result])
     {
     case SLIME:
     case SKELETON:
     case SKELETON_GENERAL:
     case WIZARD:
     case BAT:
-        if (!battle(raw_map[y_result][x_result])) {
+        if (!battle(raw_map[layer-1][y_result][x_result])) {
             printf("\n与其战斗将会死亡           \n");
             printf("%c[%d;%dH",27,18,1);
             can_move = false;
@@ -407,7 +412,7 @@ bool move() {
     case RED_GATE:
     case BLUE_GATE:
     case YELLOW_GATE:
-        if (!open_door(raw_map[y_result][x_result])){
+        if (!open_door(raw_map[layer-1][y_result][x_result])){
             printf("\n钥匙数量不足              \n");
             printf("%c[%d;%dH",27,18,1);
             can_move = false;
@@ -418,9 +423,9 @@ bool move() {
     case YELLOW_KEY:
     case BLUE_KEY:
     case RED_KEY:
-        add_key_num(raw_map[y_result][x_result]);
-        raw_map[y_result][x_result] = ROAD;
-        strcpy(map[y_result][x_result], "  ");
+        add_key_num(raw_map[layer-1][y_result][x_result]);
+        raw_map[layer-1][y_result][x_result] = ROAD;
+        strcpy(map[layer-1][y_result][x_result], "  ");
         update = true;
         break;
     case RED_BOTTLE:
@@ -464,15 +469,15 @@ bool move() {
         break;
     }
     if (update) {
-        strcpy(map[y_result][x_result], "  ");
-        raw_map[y_result][x_result] = ROAD;
+        strcpy(map[layer-1][y_result][x_result], "  ");
+        raw_map[layer-1][y_result][x_result] = ROAD;
         if (ret) {
             char buf[10];
-            strcpy(buf, map[y_result][x_result]);
+            strcpy(buf, map[layer-1][y_result][x_result]);
             print_yellow(x_result, y_result, "✦ ");
             draw();
             sleep(150);
-            strcpy(map[y_result][x_result], buf);
+            strcpy(map[layer-1][y_result][x_result], buf);
             draw();
             sleep(150);
             print_yellow(x_result, y_result, "✦ ");
@@ -484,7 +489,7 @@ bool move() {
     if (can_move && !new_layer) {
         hero.x = x_result;
         hero.y = y_result;
-        strcpy(map[y][x], "  ");
+        strcpy(map[layer-1][y][x], "  ");
         if (!ret && !new_layer)
             print_green(x_result, y_result, "勇");
         printf("\n                                \n"); 
