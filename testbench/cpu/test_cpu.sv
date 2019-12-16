e770353bc28fc0a7e59c6ac2cda51b1a75c26047
胡锦毅 <hujy17@mails.tsinghua.edu.cn>
2019-12-16 16:52:02 +0800

    Modify ci for auto-test

--- testbench/cpu/test_cpu.sv
+++ testbench/cpu/test_cpu.sv
@@ -3,8 +3,8 @@
 `define PATH_PREFIX1 "//Mac/Home/Desktop/MarcoMips/testbench/cpu/testcase/"
 module test_cpu();
 
-logic cpu_clk, peri_clk;
 logic rst, mem_stall;
+logic clk_50M;
 logic[`INST_WIDTH-1:0] instr;
 logic[`ADDR_WIDTH-1:0] pc, mem_addr;
 logic[`DATA_WIDTH-1:0] mem_wdata, mem_rdata;
@@ -13,10 +13,10 @@ logic[4:0] mem_ctrl_signal;
 logic [`INST_WIDTH - 1:0] inst_mem[256:0];
 
 initial begin
-    cpu_clk = 1'b0; # 10;
-    cpu_clk = 1'b1; # 10;
+    clk_50M = 1'b0; # 10;
+    clk_50M = 1'b1; # 10;
     forever begin
-        cpu_clk = ~cpu_clk; # 10;
+        clk_50M = ~clk_50M; # 10;
     end
 end
 
@@ -29,9 +29,10 @@ forever begin
     #20;
     end
 end
+
 cpu_core cpu(
     .instruction(instr),
-    .clk_50M(cpu_clk),
+    .clk_50M(clk_50M),
     .reset_btn(rst),
     .pc_out(pc),
 
@@ -45,7 +46,6 @@ cpu_core cpu(
 task judge(input integer fans, input integer cycle, input string out, input check_cyc);
 	string ans, out_with_cyc;
 	$fscanf(fans, "%s\n", ans);
-	
 	$sformat(out_with_cyc, "[%0d]%s", cycle, out);
 	if(check_cyc) out = out_with_cyc;
 	if(out != ans && ans != "skip")
@@ -69,6 +69,7 @@ task unittest(
 	input fpu
 );
 	integer i, fans, count, mem_index = 0;
+	integer begin_count = 0;
 	string ans, out, info;
 	for(i = 0; i < $size(inst_mem); i = i + 1)
 	inst_mem[i] = 32'h0;
@@ -87,13 +88,13 @@ task unittest(
 	count = 0;
 	//instr = inst_mem[count];
 	while(!$feof(fans))
-	begin @(negedge cpu_clk);
-	    count = count + 1;
+	begin @(negedge clk_50M);
+		begin_count = begin_count + 1;
+		count = count + 1;
 	    if (cpu.wb_reg_waddr && cpu.wb_reg_wdata)
 	    begin
-		$sformat(out, "$%0d=0x%x", cpu.wb_reg_waddr, cpu.wb_reg_wdata);
-		judge(fans, count, out, check_cyc);
-		#20;
+			$sformat(out, "$%0d=0x%x", cpu.wb_reg_waddr, cpu.wb_reg_wdata);
+			judge(fans, count, out, check_cyc);
 		end
 	end
 	$display("[OK] %0s\n", file_name);
